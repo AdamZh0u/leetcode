6a0ef9a7aa89ecc33623d159114112f7a9f531c5
AdamZh0u <zhouzz400@gmail.com>
2024-12-05 19:36:24 +0000

    add 2337

---
+++ src/problems/2337.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        if start == target:
+            return True
+        waitL = 0   
+        waitR = 0    
+
+        for curr, goal in zip(start, target):
+            if curr == 'R':
+                if waitL > 0:
+                    return False
+                waitR += 1  
+            if goal == 'L':
+                if waitR > 0:
+                    return False
+                waitL += 1
+            if goal == 'R':
+                if waitR == 0:
+                    return False
+                waitR -= 1 
+            if curr == 'L':
+                if waitL == 0:
+                    return False
+                waitL -= 1    
+        return waitL == 0 and waitR == 0
+
+            
+
+
+        
+if __name__ == '__main__':
+    start = "_L__R__R__"
+    target = 'L______RR'
+    print(Solution().canChange(start, target))
